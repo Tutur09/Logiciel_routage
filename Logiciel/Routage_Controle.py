5d3ddcc90b5d8fd0a332d07a1a47b0a329a5561d
Arthur Lhoste <arthur@lhoste.org>
2025-01-19 21:47:23 +0100

    rqm

--- Logiciel/Routage_Controle.py
+++ Logiciel/Routage_Controle.py
@@ -1,13 +1,14 @@
 import Routage_calcul as rc
 import Routage_Vent as rv
 import Routage_Paramètres as p  
-p.position_initiale, p.position_finale = rv.point_ini_fin(p.loc_nav)
 
-p.enable_prints()
+# p.position_initiale, p.position_finale = rv.point_ini_fin(p.loc_nav)
 
-rc.itere_jusqua_dans_enveloppe(
-    p.position_initiale, p.position_finale, 
-    p.pas_temporel, p.pas_angle, p.tolerance_arrivée, 
-    p.loc_nav, live = p.live, 
-    enregistrement = p.enregistrement
-    )
+# p.enable_prints()
+
+# rc.itere_jusqua_dans_enveloppe(
+#     p.position_initiale, p.position_finale, 
+#     p.pas_temporel, p.pas_angle, p.tolerance_arrivée, 
+#     p.loc_nav, live = p.live, 
+#     enregistrement = p.enregistrement
+#     )
